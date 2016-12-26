3b80db9a2a2d3ce1b803dfc1daa975e5aa01f5e4
Yutaka Tajika <y.tajika529@gmail.com>
2016-12-27 08:02:07 +0900

    Initial Commit

---
+++ Matrix/Matrix.h
@@ -0,0 +1,19 @@
+//
+//  Matrix.h
+//  Matrix
+//
+//  Created by 多鹿豊 on 2016/12/27.
+//  Copyright © 2016年 engideer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Matrix.
+FOUNDATION_EXPORT double MatrixVersionNumber;
+
+//! Project version string for Matrix.
+FOUNDATION_EXPORT const unsigned char MatrixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Matrix/PublicHeader.h>
+
+
